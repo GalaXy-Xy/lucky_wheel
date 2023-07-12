6a00eca927f827dd1a625f4bf19ce1dd8255d7b2
GalaXy-Andromeda <galaxyinblue@gmail.com>
2023-07-12 09:30:00 +0800

    feat: implement error boundary and loading spinner components for better error handling and user experience

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { AlertTriangle, RefreshCw } from 'lucide-react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null, errorInfo: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    });
+    
+    // Log error to console in development
+    if (process.env.NODE_ENV === 'development') {
+      console.error('Error caught by boundary:', error, errorInfo);
+    }
+  }
+
+  handleReset = () => {
+    this.setState({ hasError: false, error: null, errorInfo: null });
+    window.location.reload();
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="min-h-screen bg-gray-50 flex items-center justify-center px-4">
+          <div className="max-w-md w-full">
+            <div className="card text-center">
+              <div className="w-16 h-16 bg-red-100 rounded-full flex items-center justify-center mx-auto mb-4">
+                <AlertTriangle className="w-8 h-8 text-red-600" />
+              </div>
+              
+              <h1 className="text-2xl font-bold text-gray-900 mb-4">Something went wrong</h1>
+              
+              <p className="text-gray-600 mb-6">
+                We encountered an unexpected error. Please try refreshing the page or contact support if the problem persists.
+              </p>
+              
+              <div className="space-y-3">
+                <button
+                  onClick={this.handleReset}
+                  className="btn-primary w-full flex items-center justify-center space-x-2"
+                >
+                  <RefreshCw size={16} />
+                  <span>Refresh Page</span>
+                </button>
+                
+                <button
+                  onClick={() => window.history.back()}
+                  className="btn-secondary w-full"
+                >
+                  Go Back
+                </button>
+              </div>
+              
+              {process.env.NODE_ENV === 'development' && this.state.error && (
+                <details className="mt-6 text-left">
+                  <summary className="cursor-pointer text-sm font-medium text-gray-700 hover:text-gray-900">
+                    Error Details (Development)
+                  </summary>
+                  <div className="mt-2 p-3 bg-gray-100 rounded-md text-xs font-mono text-gray-800 overflow-auto">
+                    <div className="mb-2">
+                      <strong>Error:</strong> {this.state.error.toString()}
+                    </div>
+                    <div>
+                      <strong>Stack:</strong> {this.state.errorInfo.componentStack}
+                    </div>
+                  </div>
+                </details>
+              )}
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
